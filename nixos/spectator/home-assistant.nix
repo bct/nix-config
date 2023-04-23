6bf6a91bbf681ff0edd14eeeedc705df6b006d97
Brendan Taylor <bct@diffeq.com>
2023-04-23 10:39:18 -0600

    spectator: add some more components to home assistant

--- nixos/spectator/home-assistant.nix
+++ nixos/spectator/home-assistant.nix
@@ -1,11 +1,21 @@
 { inputs, lib, config, pkgs, ... }: {
   services.home-assistant = {
     enable = true;
+
     extraComponents = [
       # Components required to complete the onboarding
       "met"
       "radio_browser"
+
+      "octoprint"
+      "kodi"
+      "zha"
+
+      # Printer
+      "syncthru"
+      "ipp"
     ];
+
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
