b9bfecebc9ad5077b32549e1d6f189478b766679
Brendan Taylor <bct@diffeq.com>
2023-05-02 22:07:37 -0600

    use the 22.11 release of home-manager
    using home-manager as a nixos module was failing.
    
    according to https://github.com/nix-community/home-manager/issues/3928
    home-manager should use the same release as nixpkgs.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
