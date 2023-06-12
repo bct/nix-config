d6c4c81dc6c8f2ef9f25654107c0b12343eecd9f
Brendan Taylor <bct@diffeq.com>
2023-06-11 22:13:21 -0600

    spectator: split rtlamr into a separate file

--- nixos/spectator/configuration.nix
+++ nixos/spectator/configuration.nix
@@ -11,6 +11,7 @@
     # You can also split up your configuration and import pieces of it here:
     ../users.nix
     ./home-assistant.nix
+    ./rtlamr.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
@@ -101,13 +102,7 @@
     libraspberrypi
     git
     tmux
-
-    rtl-sdr
-    rtlamr
-    rtlamr-collect
   ];
-
-  hardware.rtl-sdr.enable = true;
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
@@ -129,68 +124,6 @@
     users = {
       # Import your home-manager configuration
       bct = import ../../home-manager/base;
-    };
-  };
-
-  users.groups.rtlamr = {};
-
-  users.users = {
-    rtlamr = {
-      isSystemUser = true;
-      group = "rtlamr";
-      extraGroups = ["plugdev"];
-    };
-  };
-
-  systemd.services.rtl_tcp = {
-    description = "rtl_tcp";
-    wantedBy = ["multi-user.target"];
-
-    serviceConfig = {
-      Type = "simple";
-      StandardOutput = "journal";
-      ExecStart = "${pkgs.rtl-sdr}/bin/rtl_tcp";
-      User = "rtlamr";
-      Group = "plugdev";
-    };
-  };
-
-  systemd.services.rtlamr-collect = {
-    description = "RTLAMR Collector";
-    environment = {
-      RTLAMR_FORMAT = "json";
-      RTLAMR_MSGTYPE = "scm";
-      RTLAMR_SERVER = "localhost:1234";
-      RTLAMR_FILTERID= "40010397,41946625";
-
-      # COLLECT_LOGLEVEL = "Debug";
-      COLLECT_INFLUXDB_HOSTNAME = "http://db.domus.diffeq.com:8086/";
-      COLLECT_INFLUXDB_ORG = "arbitrary";
-      COLLECT_INFLUXDB_BUCKET = "rtlamr";
-      COLLECT_INFLUXDB_MEASUREMENT = "utilities";
-    };
-
-    wantedBy = ["multi-user.target"];
-    bindsTo = ["rtl_tcp.service"];
-    after = ["rtl_tcp.service"];
-
-    serviceConfig = {
-      WorkingDirectory = "/run/rtlamr-collect";
-      RuntimeDirectory = "rtlamr-collect";
-      EnvironmentFile = "/run/agenix/rtlamr-collect-env";
-      ExecStart = ''/bin/sh -c "${pkgs.rtlamr}/bin/rtlamr | ${pkgs.rtlamr-collect}/bin/rtlamr-collect"'';
-      Restart = "always";
-      RestartSec = "30";
-      User = "rtlamr";
-    };
-  };
-
-  age.secrets = {
-    rtlamr-collect-env = {
-      file = ../../secrets/rtlamr-collect-env.age;
-      owner = "rtlamr";
-      group = "rtlamr";
-      mode = "600";
     };
   };
 
---
+++ nixos/spectator/rtlamr.nix
@@ -0,0 +1,71 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    rtl-sdr
+    rtlamr
+    rtlamr-collect
+  ];
+
+  hardware.rtl-sdr.enable = true;
+
+  users.groups.rtlamr = {};
+
+  users.users = {
+    rtlamr = {
+      isSystemUser = true;
+      group = "rtlamr";
+      extraGroups = ["plugdev"];
+    };
+  };
+
+  systemd.services.rtl_tcp = {
+    description = "rtl_tcp";
+    wantedBy = ["multi-user.target"];
+
+    serviceConfig = {
+      Type = "simple";
+      StandardOutput = "journal";
+      ExecStart = "${pkgs.rtl-sdr}/bin/rtl_tcp";
+      User = "rtlamr";
+      Group = "plugdev";
+    };
+  };
+
+  systemd.services.rtlamr-collect = {
+    description = "RTLAMR Collector";
+    environment = {
+      RTLAMR_FORMAT = "json";
+      RTLAMR_MSGTYPE = "scm";
+      RTLAMR_SERVER = "localhost:1234";
+      RTLAMR_FILTERID= "40010397,41946625";
+
+      # COLLECT_LOGLEVEL = "Debug";
+      COLLECT_INFLUXDB_HOSTNAME = "http://db.domus.diffeq.com:8086/";
+      COLLECT_INFLUXDB_ORG = "arbitrary";
+      COLLECT_INFLUXDB_BUCKET = "rtlamr";
+      COLLECT_INFLUXDB_MEASUREMENT = "utilities";
+    };
+
+    wantedBy = ["multi-user.target"];
+    bindsTo = ["rtl_tcp.service"];
+    after = ["rtl_tcp.service"];
+
+    serviceConfig = {
+      WorkingDirectory = "/run/rtlamr-collect";
+      RuntimeDirectory = "rtlamr-collect";
+      EnvironmentFile = "/run/agenix/rtlamr-collect-env";
+      ExecStart = ''/bin/sh -c "${pkgs.rtlamr}/bin/rtlamr | ${pkgs.rtlamr-collect}/bin/rtlamr-collect"'';
+      Restart = "always";
+      RestartSec = "30";
+      User = "rtlamr";
+    };
+  };
+
+  age.secrets = {
+    rtlamr-collect-env = {
+      file = ../../secrets/rtlamr-collect-env.age;
+      owner = "rtlamr";
+      group = "rtlamr";
+      mode = "600";
+    };
+  };
+}
