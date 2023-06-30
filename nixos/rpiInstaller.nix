1721ec867e3b0f86ef56161ecd8b6aadd1dd1815
Brendan Taylor <bct@diffeq.com>
2023-06-30 09:27:31 -0600

    rpiInstaller can use the new modules

--- nixos/rpiInstaller.nix
+++ nixos/rpiInstaller.nix
@@ -1,6 +1,7 @@
 args@{ inputs, outputs, lib, config, pkgs, ... }: {
   imports = [
-    ./users.nix
+    ./common/nix.nix
+    ./common/headless.nix
   ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
@@ -8,26 +9,4 @@ args@{ inputs, outputs, lib, config, pkgs, ... }: {
 
   # add some swap to try to speed up nixos-rebuild
   swapDevices = [ { device = "/var/lib/swapfile"; size = 1*1024; } ];
-
-  environment.systemPackages = [
-    pkgs.vim
-    pkgs.git
-  ];
-
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-    };
-  };
-
-  nix = {
-    settings = {
-      # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
-      auto-optimise-store = true;
-    };
-  };
 }
