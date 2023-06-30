652164ec0c30fba31019028bd4de2a857840d571
Brendan Taylor <bct@diffeq.com>
2023-06-30 17:34:12 -0600

    nixos/hardware/raspberry-pi/hifiberry-dac-plus.nix

---
+++ nixos/hardware/raspberry-pi/hifiberry-dac-plus.nix
@@ -0,0 +1,52 @@
+{ pkgs, ... }:
+
+{
+  sound.enable = true;
+  sound.extraConfig = ''
+    defaults.pcm.!card 1
+    defaults.ctl.!card 1
+  '';
+
+   # We need to use the vendor kernel, mainline doesn't have a driver for the HifiBerry DAC+.
+  boot.kernelPackages = pkgs.linuxPackages_rpi3;
+
+  hardware.deviceTree = {
+    enable = true;
+    overlays = [
+      {
+        name = "hifiberry-dacplus";
+        dtsFile = ./hifiberry-dacplus-overlay.dts;
+      }
+    ];
+  };
+
+  # The vendor kernel won't boot if this module loads:
+  # https://github.com/NixOS/nixpkgs/issues/200326
+  #
+  # Fortunately I don't need wifi.
+  boot.blacklistedKernelModules = [ "brcmfmac" ];
+
+  nixpkgs = {
+    overlays = [
+      # fix for kernel build failure:
+      #
+      #     modprobe: FATAL: Module ahci not found in directory
+      #
+      # https://github.com/NixOS/nixpkgs/issues/154163
+      (final: super: {
+        makeModulesClosure = x:
+          super.makeModulesClosure (x // { allowMissing = true; });
+      })
+
+      # fdtoverlay does not support the DTS overlay file I'm using below
+      # (it exits with FDT_ERR_NOTFOUND)
+      # https://github.com/raspberrypi/firmware/issues/1718
+      #
+      # we'll use dtmerge instead.
+      # https://github.com/NixOS/nixos-hardware/blob/master/raspberry-pi/4/pkgs-overlays.nix
+      (_final: prev: {
+        deviceTree.applyOverlays = prev.callPackage ./apply-overlays-dtmerge.nix { };
+      })
+    ];
+  };
+}
--- nixos/stereo/configuration.nix
+++ nixos/stereo/configuration.nix
@@ -6,31 +6,8 @@ args@{ inputs, outputs, lib, config, pkgs, options, ... }: {
     ./hardware-configuration.nix
 
     ../hardware/raspberry-pi
+    ../hardware/raspberry-pi/hifiberry-dac-plus
   ];
-
-  nixpkgs = {
-    overlays = [
-      # fix for kernel build failure:
-      #
-      #     modprobe: FATAL: Module ahci not found in directory
-      #
-      # https://github.com/NixOS/nixpkgs/issues/154163
-      (final: super: {
-        makeModulesClosure = x:
-          super.makeModulesClosure (x // { allowMissing = true; });
-      })
-
-      # fdtoverlay does not support the DTS overlay file I'm using below
-      # (it exits with FDT_ERR_NOTFOUND)
-      # https://github.com/raspberrypi/firmware/issues/1718
-      #
-      # we'll use dtmerge instead.
-      # https://github.com/NixOS/nixos-hardware/blob/master/raspberry-pi/4/pkgs-overlays.nix
-      (_final: prev: {
-        deviceTree.applyOverlays = prev.callPackage ./apply-overlays-dtmerge.nix { };
-      })
-    ];
-  };
 
   networking.hostName = "stereo";
 
@@ -47,33 +24,8 @@ args@{ inputs, outputs, lib, config, pkgs, options, ... }: {
 
   networking.firewall.enable = false;
 
-   # We need to use the vendor kernel, mainline doesn't have a driver for the HifiBerry DAC+.
-  boot.kernelPackages = pkgs.linuxPackages_rpi3;
-
-  # The vendor kernel won't boot if this module loads:
-  # https://github.com/NixOS/nixpkgs/issues/200326
-  #
-  # Fortunately I don't need wifi.
-  boot.blacklistedKernelModules = [ "brcmfmac" ];
-
-  hardware.deviceTree = {
-    enable = true;
-    overlays = [
-      {
-        name = "hifiberry-dacplus";
-        dtsFile = ./hifiberry-dacplus-overlay.dts;
-      }
-    ];
-  };
-
   # grant myself access to the sound card.
   users.users.bct.extraGroups = ["audio"];
-
-  sound.enable = true;
-  sound.extraConfig = ''
-    defaults.pcm.!card 1
-    defaults.ctl.!card 1
-  '';
 
   services.gonic = {
     enable = true;
