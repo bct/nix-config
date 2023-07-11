673efa4455fc42d552e161a0ebdf9517d132f79a
Brendan Taylor <bct@diffeq.com>
2023-07-11 02:44:22 -0600

    desktop: arrange lsp.lua

--- home-manager/desktop/files/lsp.lua
+++ home-manager/desktop/files/lsp.lua
@@ -1,10 +1,20 @@
 -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.nil_ls.setup{}
-require'lspconfig'.sorbet.setup{
-  cmd = { "srb", "tc", "--lsp", "--disable-watchman" }
-}
+
+-- golang
+require'lspconfig'.gopls.setup{}
+
+-- hoon
 require'lspconfig'.hoon_ls.setup{
   cmd = { "hoon-language-server", "-p", "8080", "-u", "http://127.0.0.1" }
 }
-require'lspconfig'.gopls.setup{}
+
+-- nix
+require'lspconfig'.nil_ls.setup{}
+
+-- python
+require'lspconfig'.pyright.setup{}
+
+-- ruby
+require'lspconfig'.sorbet.setup{
+  cmd = { "srb", "tc", "--lsp", "--disable-watchman" }
+}
