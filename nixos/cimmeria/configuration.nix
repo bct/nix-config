ae66eb82064520f9b9f64343194b7d57a8f5339d
Brendan Taylor <bct@diffeq.com>
2023-08-11 08:25:31 -0600

    cimmeria: enable binfmt emulation of aarch64

--- nixos/cimmeria/configuration.nix
+++ nixos/cimmeria/configuration.nix
@@ -142,6 +142,9 @@
     };
   };
 
+  # use emulation to compile aarch64 packages
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
