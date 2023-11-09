d891b49ec03cf2b6f6be9b3e228a9d57985f9ed6
Brendan Taylor <brendan@artificial.agency>
2023-11-09 12:05:08 -0700

    dunwich: pyright config

--- home-manager/desktop/files/lsp.lua
+++ home-manager/desktop/files/lsp.lua
@@ -12,9 +12,24 @@ require'lspconfig'.hoon_ls.setup{
 require'lspconfig'.nil_ls.setup{}
 
 -- python
-require'lspconfig'.pyright.setup{}
+require'lspconfig'.pyright.setup{
+  settings = {
+    python = {
+      pythonPath = "/home/brendan/.local/share/virtualenvs/py-QrVFw7Jf/bin/python",
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "openFilesOnly",
+        useLibraryCodeForTypes = true
+      }
+    }
+  }
+}
 
 -- ruby
 require'lspconfig'.sorbet.setup{
   cmd = { "srb", "tc", "--lsp", "--disable-watchman" }
 }
+
+-- keybindings
+vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
