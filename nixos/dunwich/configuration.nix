50585abb3840a9e91ba7d373e84e895121ffffb1
Brendan Taylor <brendan@artificial.agency>
2023-11-10 15:56:33 -0700

    dunwich: turn on GPU power management

--- nixos/dunwich/configuration.nix
+++ nixos/dunwich/configuration.nix
@@ -31,11 +31,10 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
-  #hardware.opengl = {
-    #enable = true;
-    #driSupport = true;
-    #driSupport32Bit = true;
-  #};
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+  };
 
   services.xserver.videoDrivers = ["nvidia"];
 
@@ -44,11 +43,11 @@
     modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    #powerManagement.enable = true;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    #powerManagement.finegrained = true;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -63,9 +62,6 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #package = config.boot.kernelPackages.nvidiaPackages.stable;
-
     prime = {
       # "reverse PRIME".
       # The Intel/AMD GPU will be used for all rendering, while enabling output
@@ -79,12 +75,11 @@
       # used.
       reverseSync.enable = true;
 
-      #offload = {
-        #enable = true;
-        #enableOffloadCmd = true;
-      #};
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
 
-      # TODO: Make sure to use the correct Bus ID values for your system!
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
