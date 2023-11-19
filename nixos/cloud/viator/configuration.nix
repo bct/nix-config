31f045412d8e52b32717bb5be8500aaa3bb98ce5
Brendan Taylor <bct@diffeq.com>
2023-11-18 18:05:35 -0700

    nixos/cloud/viator: split off wireguard module

--- nixos/cloud/viator/configuration.nix
+++ nixos/cloud/viator/configuration.nix
@@ -1,4 +1,4 @@
-{ self, inputs, config, pkgs, ... }: {
+{ self, inputs, pkgs, ... }: {
   imports = [
     inputs.agenix.nixosModules.default
 
@@ -8,6 +8,7 @@
     "${self}/nixos/hardware/vultr"
 
     ./coredns-wgsd.nix
+    ./wireguard.nix
   ];
 
   networking.hostName = "viator";
@@ -15,74 +16,6 @@
   time.timeZone = "Etc/UTC";
 
   system.stateVersion = "23.05";
-
-  networking.nat = {
-    enable = true;
-    internalInterfaces = ["wg0"];
-    externalInterface = "ens3";
-  };
-
-  networking.firewall.allowedUDPPorts = [ 51820 ];
-
-  environment.systemPackages = with pkgs; [
-    wireguard-tools
-  ];
-
-  systemd.network = {
-    netdevs."20-wg0" = {
-      netdevConfig = {
-        Kind = "wireguard";
-        Name = "wg0";
-      };
-      wireguardConfig = {
-        ListenPort = 51820;
-        PrivateKeyFile = config.age.secrets.viator-wireguard-key.path;
-      };
-      wireguardPeers = [
-        # router
-        {
-          wireguardPeerConfig.PublicKey = "pZ8XBJYx9gWz7emqNDkmBF+BMQ9IW9ESHCfZEj75VHw=";
-          wireguardPeerConfig.AllowedIPs = [ "192.168.8.1/32" "192.168.0.0/16" ];
-        }
-
-        # Galaxy A52 5G (2023)
-        {
-          wireguardPeerConfig.PublicKey = "rOiHdUBgYlUYXyohifWCwyyrG9XusIHQsId9OcsZJGE=";
-          wireguardPeerConfig.AllowedIPs = [ "192.168.8.2/32" ];
-        }
-
-        # cimmeria
-        {
-          wireguardPeerConfig.PublicKey = "Dr++eMTOCnbCFsCsOxTEMxornygk0hVOwlaUGww9fkk=";
-          wireguardPeerConfig.AllowedIPs = [ "192.168.8.4/32" ];
-        }
-
-        # dunwich
-        {
-          wireguardPeerConfig.PublicKey = "JHMq4bUoPeJ2LqoHkOvhvEtiYooegA7/2XXwl7604zQ=";
-          wireguardPeerConfig.AllowedIPs = [ "192.168.8.5/32" ];
-        }
-      ];
-    };
-
-    networks."20-wg0" = {
-      matchConfig.Name = "wg0";
-      address = ["192.168.8.3/16"];
-
-      # use the home router for DNS.
-      dns = ["192.168.8.1"];
-      domains = ["~domus.diffeq.com"];
-    };
-  };
-
-  age.secrets = {
-    viator-wireguard-key = {
-      file = ../../../secrets/viator-wireguard-key.age;
-      owner = "systemd-network";
-      group = "systemd-network";
-      mode = "600";
-    };
-  };
 
   systemd.services.imap-jump-socat = {
     description = "Forwards IMAP via socat";
