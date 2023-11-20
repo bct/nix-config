0f94cfd66329bbbec36346561a51b2fc9af2ac90
Brendan Taylor <bct@diffeq.com>
2023-11-19 21:55:08 -0700

    xmonad/Workspaces.hs: hardcode remote username

--- home-manager/desktop/files/xmonad/Workspaces.hs
+++ home-manager/desktop/files/xmonad/Workspaces.hs
@@ -83,10 +83,10 @@ topicConfig = TopicConfig
         [
           ("?",            spawnShell >>
                            spawn "alacritty -e htop")
-        , (miGo,           spawn "alacritty -e ssh mi-go.domus.diffeq.com")
+        , (miGo,           spawn "alacritty -e ssh bct@mi-go.domus.diffeq.com")
         , (web,            spawn "chromium")
         , (project,        spawnShell >*> 5)
-        , (mail,           spawn "alacritty -e ssh mail.domus.diffeq.com")
+        , (mail,           spawn "alacritty -e ssh bct@mail.domus.diffeq.com")
         ]
     }
 
