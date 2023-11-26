2a7d6614b32a842a929d191f0fe7407737a8bb8b
Brendan Taylor <bct@diffeq.com>
2023-11-26 07:18:16 -0700

    nixos/lan/stereo: use acme-zoneedit

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,15 @@
 let
+  # user keys
   bct-cimmeria = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAu6kY7KruzDByDYDx3k8NYe3pMxROrRxxb4rWVNfxfx5pu4Gp/fncXhK4kymxer8xbyOWBLnigHcFbdlgoIhVL7SgKGfziIrfN+cYMflKCyY5OznfPBJSD2OHiMPpQ4a/SIDrl/P6N+SqVebku4ay7N8aL62dD3xquTM5nw+3FysXsy5d1Wv/p3zt3lR3zxKYoC6gPaxUEuX7c1MRO+8VtFPge9f/Pmi2oWSjx8dKspeU83Ur/HwZfZdUSN8ABWAsV80DCI15LjlM81oyehQJBdl4AMTCApNioPPtZexYkf0UwGXh9PhMfP9cKC/dUbAfbh2eS/ReteC81G1MPPf9C3AzxDBh/k4iIarTU6pTgumSaV5rSvYxtjpxaRP4cbRKgNPu8n6XMrUM9cRL+ozBXBmqYWzuzY2QATmVadKZddx3SSoiAtakMLvASGh5RW/hDxm6td49jF4U7XB9sC3dAkRkJFSf91PYeiBgJXzNvsxGlwpJFMmv5vmDPN+HZTU=";
 
+  # host keys
+  #   ssh-keyscan -t ssh-ed25519 <host>
+
+  # -- lan
   spectator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHwG+kqbUaFI2xwHZO76CrQh5+YnElQsjB6DOWNtMc1e";
+  stereo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+0o3CDs78/NW73QxiZ4gJtXgZ5U+NAu8o9lNhzmLwl";
+
+  # -- cloud
   s3-proxy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/edQPh5lgflnMjVHAHhRDNNmmusQxm7MHU2QE7kiyV";
   notes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgWQElHPbvswEcaYNUAQ1E8Kw/KL4e3H4VrGicBNxAJ";
   viator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHEtc5zAnIK1S6p+8L/FNjYgujVqhGUUG4Y9WpYb06mO";
@@ -23,4 +31,7 @@ in
 
     # SSH keys for accessing borg.domus.diffeq.com
     "notes-borg-ssh-key.age".publicKeys = [ bct-cimmeria notes ];
+
+    # ZoneEdit API key for creating TXT records for ACME
+    "zoneedit.age".publicKeys = [ bct-cimmeria spectator stereo ];
   }
