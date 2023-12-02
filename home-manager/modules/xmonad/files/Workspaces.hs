9334df3d184eae20453fb92de1268a2d3644cc22
Brendan Taylor <bct@diffeq.com>
2023-12-02 08:21:04 -0700

    xmonad: split off "extraWorkspaces"

--- home-manager/modules/xmonad/files/Workspaces.hs
+++ home-manager/modules/xmonad/files/Workspaces.hs
@@ -6,88 +6,72 @@ import XMonad.Actions.SpawnOn (spawnHere)
 import XMonad.Actions.TopicSpace
 
 import qualified Data.Map as M
+import Data.Maybe (mapMaybe)
 
--- icons
--- https://www.nerdfonts.com/cheat-sheet
-iconQuestionMark :: String
-iconQuestionMark = "\xf128"
+type ExtraWorkspace = (String, String, Maybe String, Maybe (X ()))
 
-iconGlobe :: String
-iconGlobe = "\xeb01"
+extraWorkspaces :: [ExtraWorkspace]
+extraWorkspaces = [
+  -- icon, name, dir, action
+  -- https://www.nerdfonts.com/cheat-sheet
 
-iconBeaker :: String
-iconBeaker = "\xf499"
+    -- beaker
+    ("\xf499", "project", Just "projects", Just $ spawnShell >*> 5)
 
-iconShipWheel :: String
-iconShipWheel = "\xf0833"
+   -- ship wheel
+  , ("\xf0833", "mi-go", Nothing, Just $ spawn "alacritty -e ssh bct@mi-go.domus.diffeq.com")
 
-iconWireframeCube :: String
-iconWireframeCube = "\xf01a7"
+    -- nf-md-email
+  , ("\xf01ee", "mail", Nothing, Just $ spawn "alacritty -e ssh bct@mail.domus.diffeq.com")
 
-iconFilm :: String
-iconFilm = "\xf008"
+    -- wireframe cube
+  , ("\xf01a7", "slicer", Nothing, Just $ spawn "cura")
 
-iconMail :: String
-iconMail = "\xf01ee"
-
--- topic names
-project :: String
-project = "project"
-
-miGo :: String
-miGo = "mi-go"
-
-slicer :: String
-slicer = "slicer"
+    -- film
+  , ("\xf008", "kino", Nothing, Nothing)
+  ]
 
 web :: String
 web = "web"
-
-kino ::String
-kino = "kino"
-
-mail :: String
-mail = "mail"
 
 topicNameToIcon :: String -> String
 topicNameToIcon name = M.findWithDefault name name iconMap ++ " "
   where
     iconMap :: M.Map String String
-    iconMap = M.fromList [
-                 ("?",     " " ++ iconQuestionMark)
-                ,(web,     iconGlobe)
-                ,(project, iconBeaker)  -- beaker
-                ,(miGo,    iconShipWheel) -- ship wheel
-                ,(slicer,  iconWireframeCube) -- wireframe cube
-                ,(kino,    iconFilm)  -- film
-                ,(mail,    iconMail)  -- nf-md-email
-              ]
+    iconMap = M.fromList $ [
+                 ("?",     " \xf128") -- question mark
+                ,(web,     "\xeb01")  -- globe
+              ] ++ map (\(icon, name, _, _) -> (name, icon)) extraWorkspaces
 
 topicNameWithIconPrefix :: String -> String
 topicNameWithIconPrefix name = (topicNameToIcon name) ++ " " ++ name
 
 topics :: [Topic]
-topics = ["?", web, project, miGo, slicer, mail, kino]
+topics = ["?", web] ++ map (\(icon, name, _, _) -> name) extraWorkspaces
+
+extraTopicDirs :: [(String, String)]
+extraTopicDirs = mapMaybe doMap extraWorkspaces
+  where
+    doMap :: ExtraWorkspace -> Maybe (String, String)
+    doMap = (\(_, name, maybeDir, _) -> fmap (\dir -> (name, dir)) maybeDir)
+
+extraTopicActions :: [(String, X ())]
+extraTopicActions = mapMaybe doMap extraWorkspaces
+  where
+    doMap :: ExtraWorkspace -> Maybe (String, X())
+    doMap = (\(_, name, _, maybeAct) -> fmap (\act -> (name, act)) maybeAct)
 
 topicConfig :: TopicConfig
 topicConfig = TopicConfig
-    { topicDirs = M.fromList $
-        [ (project,   "projects")
-        ]
-
+    { topicDirs = M.fromList extraTopicDirs
     , defaultTopicAction = const spawnShell
-
-    , defaultTopic = project
-
+    , defaultTopic = "project"
     , topicActions = M.fromList $
         [
           ("?",            spawnShell >>
                            spawn "alacritty -e htop")
-        , (miGo,           spawn "alacritty -e ssh bct@mi-go.domus.diffeq.com")
         , (web,            spawn "chromium")
-        , (project,        spawnShell >*> 5)
-        , (mail,           spawn "alacritty -e ssh bct@mail.domus.diffeq.com")
-        ]
+        ] ++ extraTopicActions
     }
 
 spawnShell :: X ()
