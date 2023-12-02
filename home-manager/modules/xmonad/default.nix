8ce1ed57d3224240387332a1797570a488218220
Brendan Taylor <bct@diffeq.com>
2023-12-02 07:20:50 -0700

    split off xmonad module

---
+++ home-manager/modules/xmonad/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  xsession.windowManager = {
+    xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      config = ./files/xmonad.hs;
+      libFiles = {
+        "Workspaces.hs" = ./files/Workspaces.hs;
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    xmobar
+
+    (nerdfonts.override { fonts = [ "UbuntuMono" ]; })
+    ubuntu_font_family
+
+    alacritty
+    dmenu
+    light
+  ];
+}
