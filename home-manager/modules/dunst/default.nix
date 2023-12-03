9d643f0a5ecebf6e9bdf8e480273e315152a11d0
Brendan Taylor <bct@diffeq.com>
2023-12-03 15:48:59 -0700

    dunst: gruvbox

--- home-manager/modules/dunst/default.nix
+++ home-manager/modules/dunst/default.nix
@@ -7,6 +7,25 @@
     settings = {
       global = {
         font = "UbuntuMono Nerd Font 18";
+        frame_color = "#83a598";
+
+        origin = "bottom-right";
+        offset = "10x10";
+      };
+
+      urgency_low = {
+        background = "#282828";
+        foreground = "#ebdbb2";
+      };
+
+      urgency_normal = {
+        background = "#83a598";
+        foreground = "#282828";
+      };
+
+      urgency_critical = {
+        background = "#fabd2f";
+        foreground = "#282828";
       };
     };
   };
