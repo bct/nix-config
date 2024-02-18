13ac95b43bed982151903e83ac90bcc7e9d371ad
Brendan Taylor <bct@diffeq.com>
2024-02-18 09:31:31 -0700

    home-manager/desktop: aliases for bringing wireguard up and down

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -92,6 +92,9 @@
 
     ls = "ls --color=auto";
     grep = "grep --color=auto";
+
+    wg-up = "sudo systemctl start wg-quick-wg0.service";
+    wg-down = "sudo systemctl stop wg-quick-wg0.service";
   };
 
   xsession = {
