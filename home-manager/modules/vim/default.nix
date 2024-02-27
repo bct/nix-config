afe01e7cf8334ee9f58658ffa1de52d9d9023cf1
Brendan Taylor <brendan@artificial.agency>
2024-02-27 16:59:16 -0700

    nvim: run conform asynchronously for python
    black is running slowly for some reason.

--- home-manager/modules/vim/default.nix
+++ home-manager/modules/vim/default.nix
@@ -116,24 +116,42 @@
           config = ''
             local conform = require 'conform'
 
+            -- not sure why black is so slow, that may require investigation
+            -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#automatically-run-slow-formatters-async
+            local slow_format_filetypes = {"python"}
+
             conform.setup({
               formatters = {
                 black = { command = "${pkgs.black}/bin/black" },
                 isort = { command = "${pkgs.isort}/bin/isort" },
                 prettier = { command = "${pkgs.nodePackages.prettier}/bin/prettier" },
               },
+
               formatters_by_ft = {
                 -- run isort, then black
                 python = { "isort", "black" },
               },
-              format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_fallback = true,
-              },
-            })
 
-            conform.format { async = true, lsp_fallback = true }
+              format_on_save = function(bufnr)
+                if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                  return
+                end
+                local function on_format(err)
+                  if err and err:match("timeout$") then
+                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
+                  end
+                end
+
+                return { timeout_ms = 200, lsp_fallback = true }, on_format
+              end,
+
+              format_after_save = function(bufnr)
+                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                  return
+                end
+                return { lsp_fallback = true }
+              end,
+            })
 
             local jsLangs = {"javascript", "json", "typescript", "typescriptreact"}
             for _, lang in ipairs(jsLangs)
