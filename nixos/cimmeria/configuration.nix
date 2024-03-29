4accf947738c428c0eee1d0080174c31adc40efa
Brendan Taylor <bct@diffeq.com>
2024-03-29 17:08:07 -0600

    nixos/cimmeria: enable trezord

--- nixos/cimmeria/configuration.nix
+++ nixos/cimmeria/configuration.nix
@@ -68,6 +68,8 @@
   hardware.opengl.driSupport32Bit = true;
   hardware.opengl.enable = true;
 
+  services.trezord.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
