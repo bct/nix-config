cb1e1ff15d017ddea07ca39e5c27de844d66ecd8
Brendan Taylor <brendan@artificial.agency>
2024-04-11 11:57:48 -0600

    home-manager/desktop: split out shell.nix

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -4,6 +4,7 @@
 
     ../modules/vim
 
+    ./shell.nix
     ./xorg.nix
   ];
 
@@ -65,65 +66,6 @@
     # media
     supersonic
   ];
-
-  home.sessionPath = [
-    "$HOME/.local/share/gem/ruby/3.0.0/bin"
-    "$HOME/bin"
-  ];
-
-  home.sessionVariables = {
-    EDITOR = "vim";
-    LANG = "en_CA.utf8";
-
-    # scale chromium/brave browser UIs.
-    # maybe I should figure out how to scale the UI more generally?
-    CHROME_EXTRA_FLAGS = "--force-device-scale-factor=1.5";
-  };
-
-  home.shellAliases = {
-    aoeu = "setxkbmap -layout us";
-    asdf = "setxkbmap -layout us -variant dvorak";
-
-    ls = "ls --color=auto";
-    grep = "grep --color=auto";
-
-    wg-up = "sudo systemctl start wg-quick-wg0.service";
-    wg-down = "sudo systemctl stop wg-quick-wg0.service";
-  };
-
-  programs.bash = {
-    enable = true;
-    historyControl = ["ignoredups"];
-
-    initExtra = ''
-      # autojump
-      eval "$(${pkgs.z-lua}/bin/z --init bash)"
-
-      # go to the root of the current repository
-      r() {
-        cd "$(git rev-parse --show-toplevel 2>/dev/null)"
-      }
-
-      . ~/.nix-profile/share/git/contrib/completion/git-prompt.sh
-
-      # bct@cimmeria:~/projects/nixfiles (master) $
-      PS1='\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(__git_ps1 "(%s)")\$ '
-
-      # If this is a GUI terminal, set the title to user@host:dir
-      case "$TERM" in
-      xterm*|rxvt*|alacritty*)
-        PS1="\[\e]0;\u@\h: \w\a\]$PS1"
-          ;;
-      *)
-          ;;
-      esac
-
-      # make ^L work
-      bind -m vi-insert 'Control-l: clear-screen'
-
-      export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels''${NIX_PATH:+:$NIX_PATH}
-    '';
-  };
 
   # Raw configuration files
   home.file.".alacritty.yml".source = ./files/alacritty.yml;
