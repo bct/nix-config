03f73d0b8bd1cd1e3a4457ca9cfc052432a816d1
Brendan Taylor <bct@diffeq.com>
2024-07-21 21:16:47 -0600

    nixos/modules/wireguard-via-wgsd: use a hostname instead of an IP address

--- nixos/modules/wireguard-via-wgsd.nix
+++ nixos/modules/wireguard-via-wgsd.nix
@@ -6,13 +6,15 @@ let
 
   wireguardInterface = "wg0";
 
-  # this is viator.diffeq.com
-  # we can't use a hostname here - it's resolved after the interface is brought
-  # up, and adding the interface might have broken DNS resolution.
-  wgsdDns = "104.156.252.101:53";
+  # the DNS host that can provide information about our WireGuard peers
+  wgsdDns = "viator.diffeq.com:53";
+
+  # the DNS zone that we're discovering peers in
   wgsdZone = "diffeq.com";
 
+  # a nameserver to add to resolv.conf
   dns = "192.168.8.1";
+
   routerPublicKey = "pZ8XBJYx9gWz7emqNDkmBF+BMQ9IW9ESHCfZEj75VHw=";
   routerAllowedIps = [ "192.168.8.1/32" "192.168.0.0/20" ];
 in {
@@ -53,14 +55,25 @@ in {
       ${wireguardInterface} = {
         autostart = false;
         address = [ cfg.address ];
-        dns = [ dns ];
 
         # run manually:
         #   umask 077 && mkdir /etc/wireguard && wg genkey >/etc/wireguard/wg0.key
         privateKeyFile = "/etc/wireguard/${wireguardInterface}.key";
 
         postUp = ''
+          # this has to happen before we update our DNS configuration, so that we can
+          # look up the IP of our DNS server.
           ${pkgs.wgsd}/bin/wgsd-client -device ${wireguardInterface} -zone=${wgsdZone} -dns=${wgsdDns}
+
+          # this is manually accomplishing what the "DNS" option would accomplish.
+          # https://manpages.debian.org/unstable/wireguard-tools/wg-quick.8.en.html
+          echo "nameserver ${dns}" | ${pkgs.openresolv}/bin/resolvconf -a tun.%i -m 0 -x
+        '';
+
+        postDown = ''
+          # this is manually accomplishing what the "DNS" option would accomplish.
+          # https://manpages.debian.org/unstable/wireguard-tools/wg-quick.8.en.html
+          ${pkgs.openresolv}/bin/resolvconf -d tun.%i
         '';
 
         peers = [
