1a49a2ae0c0992e9a3f834c5e56a803f286286ed
Brendan Taylor <bct@diffeq.com>
2025-01-24 06:55:03 -0700

    yuggoth/guests/prometheus: add ping exporter

--- nixos/lan/yuggoth/guests/prometheus.nix
+++ nixos/lan/yuggoth/guests/prometheus.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   system.stateVersion = "24.05";
 
   microvm = {
@@ -63,7 +63,27 @@
           { targets = ["localhost:9817"]; }
         ];
       }
+
+      {
+        job_name = "ping";
+        scrape_interval = "60s";
+        static_configs = [
+          { targets = ["localhost:${toString config.services.prometheus.exporters.ping.port}"]; }
+        ];
+      }
     ];
+
+    exporters = {
+      ping = {
+        enable = true;
+        settings = {
+          targets = [
+            "scx-3405w.domus.diffeq.com"
+            "fever-dreams.domus.diffeq.com"
+          ];
+        };
+      };
+    };
   };
 
   # services.prometheus.exporters.graphite.enable ?
