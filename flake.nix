abbb9f0dea491918db99c7c15875d52d3c7b0afc
Brendan Taylor <bct@diffeq.com>
2025-05-24 08:40:22 -0600

    flake.nix: nixos-24.11 -> nixos-25.05

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # Branch that includes the lubelogger module
     nixpkgs-lubelogger.url = "github:bct/nixpkgs/lubelogger-module";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # flake-parts
