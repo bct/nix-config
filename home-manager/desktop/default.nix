2f251431900e6b80a79ff9c0265c74781df1dbbc
Brendan Taylor <bct@diffeq.com>
2025-11-27 08:29:10 -0700

    home-manager: add fonts

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -53,6 +53,22 @@
     # fonts
     ubuntu_font_family
     nerd-fonts.ubuntu-mono
+    dejavu_fonts
+    corefonts
+
+    # maybe prefer https://github.com/Soft/nix-google-fonts-overlay ?
+    (pkgs.google-fonts.override {
+      # https://fonts.google.com/
+      fonts = [
+        "Crimson Text"
+        "IM Fell DW Pica SC"
+        "IM Fell English"
+        "IM Fell English SC"
+        "Parisienne"
+        "Sacramento"
+        "UnifrakturMaguntia"
+      ];
+    })
 
     # terminal
     alacritty
