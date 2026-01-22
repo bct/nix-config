91cd5b023f8da710e91edc8bdb66dcb8406602a4
Brendan Taylor <bct@diffeq.com>
2026-01-22 09:36:57 -0700

    megahost-one: bump coredns hash

--- nixos/cloud/megahost-one/coredns-wgsd.nix
+++ nixos/cloud/megahost-one/coredns-wgsd.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   networking.firewall.allowedUDPPorts = [ 53 ];
 
   # set up coredns to respond to DNS requests for _wireguard._udp.diffeq.com
@@ -18,16 +19,22 @@
 
     package = pkgs.coredns.override {
       externalPlugins = [
-        {name = "wgsd"; repo = "github.com/jwhited/wgsd"; version = "v0.3.6";}
+        {
+          name = "wgsd";
+          repo = "github.com/jwhited/wgsd";
+          version = "v0.3.6";
+        }
       ];
       # the vendorHash needs to be updated every time pkgs.coredns updates,
       # due to the way the core & plugin packages are combined when vendored.
       # I spent a couple of days trying to fix this, and eventually gave up.
-      vendorHash = "sha256-4jBu/Gub3tJ2Up98V4Qg3AA3TmSeSeekwmjvH5PIHTg=";
+      vendorHash = "sha256-pauVp0pa/dSY2ACoaTRzlW6lI9DuwG3miO7uz9Cbr4k=";
     };
   };
 
   # wgsd needs cap_net_admin to read the wireguard peers
-  systemd.services.coredns.serviceConfig.CapabilityBoundingSet = pkgs.lib.mkForce "cap_net_bind_service cap_net_admin";
-  systemd.services.coredns.serviceConfig.AmbientCapabilities = pkgs.lib.mkForce "cap_net_bind_service cap_net_admin";
+  systemd.services.coredns.serviceConfig.CapabilityBoundingSet =
+    pkgs.lib.mkForce "cap_net_bind_service cap_net_admin";
+  systemd.services.coredns.serviceConfig.AmbientCapabilities =
+    pkgs.lib.mkForce "cap_net_bind_service cap_net_admin";
 }
