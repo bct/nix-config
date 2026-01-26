190653aeae93d75cc4dbd50c20c1c2fb2d800ffa
Brendan Taylor <bct@diffeq.com>
2026-01-26 16:47:57 -0700

    home-manager/modules/notifications: gruvbox

--- home-manager/modules/notifications/default.nix
+++ home-manager/modules/notifications/default.nix
@@ -12,6 +12,14 @@
       anchor = "bottom-right";
       default-timeout = 7500;
       outer-margin = "10,0";
+
+      # gruvbox
+      background-color = "#3c3836cc";
+      text-color = "#ebdbb2";
+      border-color = "#83a598cc";
+      border-radius = 6;
+      border-size = 2;
+      layer = "overlay";
     };
   };
 }
