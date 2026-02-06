483420780e263b85ae3b463bfba04bb968e62017
Brendan Taylor <bct@diffeq.com>
2026-02-06 07:55:50 -0700

    spectator: tidy home assistant a little

--- nixos/lan/yuggoth/guests/spectator/home-assistant.nix
+++ nixos/lan/yuggoth/guests/spectator/home-assistant.nix
@@ -1,4 +1,8 @@
 { pkgs, config, ... }:
+let
+  port = 8123;
+  sslDirectory = "${config.security.acme.certs."spectator.domus.diffeq.com".directory}";
+in
 {
   age.secrets = {
     home-assistant-my-cnf = {
@@ -14,7 +18,7 @@
     group = "hass";
   };
 
-  networking.firewall.allowedTCPPorts = [ 8123 ];
+  networking.firewall.allowedTCPPorts = [ port ];
 
   services.home-assistant = {
     enable = true;
@@ -47,70 +51,63 @@
         mysqlclient
       ];
 
-    config =
-      let
-        sslDirectory = "${config.security.acme.certs."spectator.domus.diffeq.com".directory}";
-      in
-      {
-        # Includes dependencies for a basic setup
-        # https://www.home-assistant.io/integrations/default_config/
-        default_config = { };
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = { };
 
-        homeassistant = {
-          time_zone = "America/Edmonton";
-          country = "CA";
-          external_url = "https://spectator.domus.diffeq.com:8123";
-          internal_url = "https://spectator.domus.diffeq.com:8123";
+      homeassistant = {
+        time_zone = "America/Edmonton";
+        country = "CA";
+        external_url = "https://spectator.domus.diffeq.com:${toString port}";
+        internal_url = "https://spectator.domus.diffeq.com:${toString port}";
+      };
+
+      http = {
+        ssl_certificate = "${sslDirectory}/fullchain.pem";
+        ssl_key = "${sslDirectory}/key.pem";
+      };
+
+      recorder = {
+        # keep data around for longer
+        purge_keep_days = 60;
+
+        # use remote mariadb
+        db_url = "mysql://home_assistant@db.domus.diffeq.com/home_assistant?charset=utf8mb4&read_default_file=${config.age.secrets.home-assistant-my-cnf.path}";
+
+        # wait up to 5 minutes for the database to come up.
+        # e.g. if there's a power outage it takes TrueNAS some time to bring
+        # the jails back up.
+        db_max_retries = 30;
+        db_retry_wait = 10;
+      };
+
+      "automation ui" = "!include automations.yaml";
+      "scene ui" = "!include scenes.yaml";
+      "script ui" = "!include scripts.yaml";
+
+      rest_command = {
+        stereo_power_toggle = {
+          url = "http://stereo.domus.diffeq.com:4646/ssap/power";
+          method = "POST";
         };
 
-        http = {
-          ssl_certificate = "${sslDirectory}/fullchain.pem";
-          ssl_key = "${sslDirectory}/key.pem";
+        stereo_line_1 = {
+          url = "http://stereo.domus.diffeq.com:4646/ssap/line-1";
+          method = "POST";
         };
 
-        recorder = {
-          # commit less frequently to reduce wear on the SD card
-          commit_interval = 30;
-
-          # keep data around for longer
-          purge_keep_days = 60;
-
-          # use remote mariadb
-          db_url = "mysql://home_assistant@db.domus.diffeq.com/home_assistant?charset=utf8mb4&read_default_file=${config.age.secrets.home-assistant-my-cnf.path}";
-
-          # wait up to 5 minutes for the database to come up.
-          # e.g. if there's a power outage it takes TrueNAS some time to bring
-          # the joils back up.
-          db_max_retries = 30;
-          db_retry_wait = 10;
+        stereo_volume_up = {
+          url = "http://stereo.domus.diffeq.com:4646/ssap/volume-up";
+          method = "POST";
         };
 
-        "automation ui" = "!include automations.yaml";
-        "scene ui" = "!include scenes.yaml";
-        "script ui" = "!include scripts.yaml";
-
-        rest_command = {
-          stereo_power_toggle = {
-            url = "http://stereo.domus.diffeq.com:4646/ssap/power";
-            method = "POST";
-          };
-
-          stereo_line_1 = {
-            url = "http://stereo.domus.diffeq.com:4646/ssap/line-1";
-            method = "POST";
-          };
-
-          stereo_volume_up = {
-            url = "http://stereo.domus.diffeq.com:4646/ssap/volume-up";
-            method = "POST";
-          };
-
-          stereo_volume_down = {
-            url = "http://stereo.domus.diffeq.com:4646/ssap/volume-down";
-            method = "POST";
-          };
+        stereo_volume_down = {
+          url = "http://stereo.domus.diffeq.com:4646/ssap/volume-down";
+          method = "POST";
         };
       };
+    };
   };
 
   services.mosquitto = {
