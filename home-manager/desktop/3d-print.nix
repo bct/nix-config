93253d8ddee20f29634741870fc335c3bcb3d893
Brendan Taylor <bct@diffeq.com>
2026-06-28 08:10:18 -0600

    scale orca-slicer's UI
    I've lost track of what's going on with xwayland scaling

--- home-manager/desktop/3d-print.nix
+++ home-manager/desktop/3d-print.nix
@@ -1,9 +1,23 @@
 { pkgs, ... }:
+let
+  # Make the UI big enough to be usable. I'm not sure why it doesn't use the same
+  # settings as everything else on Wayland. I'm sure there's a better way to do this!
+  orca-slicer = pkgs.symlinkJoin {
+    name = "orca-slicer-wrapped";
+    paths = [ pkgs.orca-slicer ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/orca-slicer \
+        --set GDK_SCALE 2
+    '';
+  };
+in
 {
   home.packages = [
     pkgs.unstable.cura-appimage
     pkgs.freecad
     pkgs.openscad
-    pkgs.orca-slicer
+
+    orca-slicer
   ];
 }
