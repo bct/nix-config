fc7f3c4f8c68f05b0f2565f4a48e7eb63d8e45c9
Brendan Taylor <bct@diffeq.com>
2026-08-04 09:18:00 -0600

    minecraft: operators are also whitelisted
    F. keeps deopping himself and losing the ability to connect to the
    server

--- nixos/cloud/megahost-one/minecraft/servers.nix
+++ nixos/cloud/megahost-one/minecraft/servers.nix
@@ -13,7 +13,8 @@ let
     Wblaikiebee = "6a3da427-b081-456a-a4cd-2c08a61e5780"; # I's friend B.
     MrDoughnutPlayz = "43d53948-7feb-4f44-9aa7-dec673e048b1"; # F.'s schoolmate B.
     GothGirlJ = "b6ce94fa-6d2a-4fb7-affd-9a8c5abb2277"; # J.
-  };
+  }
+  // operators;
   operators = {
     # drasl
     GothGirl = "137bb49c-9bf5-4479-9003-19fedacd7357"; # J.
